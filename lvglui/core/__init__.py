dc9d36789f8593b9494a8406af6b6a6611b9b5b6
YuLong Yao <feilongphone@gmail.com>
2023-09-23 20:52:47 +0800

    lvglui: core: auto import all file
    Signed-off-by: YuLong Yao <feilongphone@gmail.com>

--- lvglui/core/__init__.py
+++ lvglui/core/__init__.py
@@ -1,5 +1,15 @@
-from .arc import arc
-from .bar import bar
-from .button import button
-from .label import label
-from .screen import screen
+import os
+import importlib
+
+contents = os.listdir(os.path.dirname(__file__))
+module_files = [
+    f
+    for f in contents
+    if f.endswith(".py") and not f.startswith(("__init__", "__main__"))
+]
+
+
+for module_file in module_files:
+    module_name = os.path.splitext(module_file)[0]
+    module = importlib.import_module(f".{module_name}", package=__package__)
+    globals()[module_name] = getattr(module, module_name)
