1699f338125a55e228f7184a1e050ab5039bafcb
YuLong Yao <feilongphone@gmail.com>
2023-09-23 20:16:33 +0800

    lvglui: label: trate text set as attribute
    Signed-off-by: YuLong Yao <feilongphone@gmail.com>

--- lvglui/core/label.py
+++ lvglui/core/label.py
@@ -5,19 +5,8 @@ class Label(LvObject):
     def __init__(self, parent=None, text="", **kwargs) -> None:
         super().__init__(parent, **kwargs)
 
-        self.text = text
+        self["lv_label_set_text"] = [f'"{text}"']
 
     @property
     def create_func(self):
         return "lv_label_create"
-
-    def _setText(self) -> str:
-        return f"""	lv_label_set_text({self.name}, "{self.text}");"""
-
-    def define(self):
-        return "\n".join(
-            [
-                super().define(),
-                self._setText(),
-            ]
-        )
