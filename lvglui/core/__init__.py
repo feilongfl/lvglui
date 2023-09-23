298681402fa02d1a7e5c5f483de6518550a0dbc7
YuLong Yao <feilongphone@gmail.com>
2023-09-23 20:30:44 +0800

    lvglui: basic: support bar widget
    Signed-off-by: YuLong Yao <feilongphone@gmail.com>

--- lvglui/core/__init__.py
+++ lvglui/core/__init__.py
@@ -1,4 +1,5 @@
 from .arc import Arc
+from .bar import Bar
 from .button import Button
 from .label import Label
 from .screen import Screen
---
+++ lvglui/core/bar.py
@@ -0,0 +1,12 @@
+from ..basic import LvObject
+
+
+class Bar(LvObject):
+    def __init__(self, parent=None, value=0, **kwargs) -> None:
+        super().__init__(parent, **kwargs)
+
+        self["lv_bar_create"] = [value]
+
+    @property
+    def create_func(self):
+        return "lv_bar_create"
