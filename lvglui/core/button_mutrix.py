8576ac6ee1abf77b26c88a936d0d4840b68a8659
YuLong Yao <feilongphone@gmail.com>
2023-09-23 21:28:42 +0800

    lvglui: core: button_matrix: initial widget
    Signed-off-by: YuLong Yao <feilongphone@gmail.com>

---
+++ lvglui/core/button_mutrix.py
@@ -0,0 +1,24 @@
+from ..basic import LvObject
+
+
+class button_mutrix(LvObject):
+    def __init__(self, parent=None, button_map=[], **kwargs) -> None:
+        super().__init__(parent, **kwargs)
+
+        self["__button_map"] = button_map
+        self["lv_btnmatrix_set_map"] = [self.btnmap_name]
+
+    @property
+    def btnmap_name(self):
+        return f"{self.name}_btnmap"
+
+    @property
+    def create_func(self):
+        return "lv_btnmatrix_create"
+
+    @property
+    def raw_attribute(self):
+        return """static const char * %s[] = {%s, ""};""" % (
+            self.btnmap_name,
+            ",".join([f'"{x}"' for x in self["__button_map"]]),
+        )
