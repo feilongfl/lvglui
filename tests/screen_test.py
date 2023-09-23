4f05bf95500a8f10227e1111dc0646a4f462ecbe
YuLong Yao <feilongphone@gmail.com>
2023-09-23 20:02:05 +0800

    test: screen_test: center button and text
    Signed-off-by: YuLong Yao <feilongphone@gmail.com>

--- tests/screen_test.py
+++ tests/screen_test.py
@@ -3,10 +3,16 @@ import lvglui
 
 class DemoScreen(lvglui.core.Screen):
     def build(self):
+        self["lv_obj_set_flex_flow"] = ["LV_FLEX_FLOW_COLUMN"]
+        self["lv_obj_set_flex_align"] = [
+            "LV_FLEX_ALIGN_CENTER",
+            "LV_FLEX_ALIGN_START",
+            "LV_FLEX_ALIGN_CENTER",
+        ]
+
         self.extend(
             [
-                lvglui.core.Label(self, "Hello"),
-                lvglui.core.Label(self, "World"),
+                lvglui.core.Label(self, "Hello World"),
                 lvglui.core.Button(
                     self,
                     name="btn",
@@ -16,10 +22,6 @@ class DemoScreen(lvglui.core.Screen):
                 ),
             ]
         )
-
-    @property
-    def attributes(self):
-        return {"lv_obj_set_flex_flow": ["LV_FLEX_FLOW_COLUMN"]}
 
 
 def test_screen(caplog):
@@ -36,5 +38,6 @@ lv_obj_t *lvui_screen_create(lv_obj_t *parent)
             """
 	return %s;
 }
-""" % scn.name
+"""
+            % scn.name
         )
