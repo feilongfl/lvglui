b728940e281d593f9430acbf58c8e03580da0807
YuLong Yao <feilongphone@gmail.com>
2023-09-24 18:35:59 +0800

    lvglui: generator: tamplate: drop deprecated funcs
    Signed-off-by: YuLong Yao <feilongphone@gmail.com>

--- lvglui/generator/tamplate/lvgl_c.tpl.c
+++ lvglui/generator/tamplate/lvgl_c.tpl.c
@@ -4,7 +4,23 @@
 #include "${lvobj.name}_data.inc"
 
 lv_obj_t *lvglui_create_${lvobj.name}(lv_obj_t *parent) {
-    ${lvobj.generate()}
+% for obj in lvobj.get_children():
+    /* lvglui: ${obj.__class__.__name__}: ${obj.full_name} */
+    ${obj.typedef} ${obj.name} = ${obj.create_func}(${obj.parent});
+% if obj.raw_attribute != None:
+    /* lvglui: ${obj.__class__.__name__}: ${obj.full_name}: RAW ATTRIBUTE */
+    ${obj.raw_attribute}
+% endif
+    /* lvglui: ${obj.__class__.__name__}: ${obj.full_name}: ATTRIBUTE */
+% for attr, val in obj.attributes.items():
+% if attr.startswith("@"):
+    ${attr}(${','.join(val)});
+% elif attr.startswith("__"):
+% else:
+    ${attr}(${', '.join([obj.name, *val])});
+% endif
+% endfor
 
+% endfor
     return ${lvobj.name};
 }
