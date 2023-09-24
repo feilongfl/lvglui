147abc3de940cb1e70fe01029ea9c756bbc0055f
YuLong Yao <feilongphone@gmail.com>
2023-09-24 19:37:53 +0800

    exp

--- example/widget_demo/ui/ui.py
+++ example/widget_demo/ui/ui.py
@@ -3,22 +3,37 @@ import lvglui
 
 class DemoScreen(lvglui.core.screen):
     def build(self):
-        self["lv_obj_set_flex_flow"] = ["LV_FLEX_FLOW_COLUMN"]
+        self["lv_obj_set_flex_flow"] = ["LV_FLEX_FLOW_ROW"]
         self["lv_obj_set_flex_align"] = [
-            "LV_FLEX_ALIGN_CENTER",
             "LV_FLEX_ALIGN_START",
+            "LV_FLEX_ALIGN_CENTER",
             "LV_FLEX_ALIGN_CENTER",
         ]
 
+        roller = lvglui.core.roller(
+            self,
+            [
+                "Arc",
+                "Bar",
+                "Button",
+                "Button Matrix",
+                "Canvas",
+                "CheckBox",
+                "DropDown",
+                "image",
+                "Label",
+                "Line",
+                "Roller",
+                "Slider",
+                "Switch",
+                "Table",
+                "TextArea",
+            ],
+        )
+        roller["lv_roller_set_visible_row_count"] = 6
+
         self.extend(
             [
-                lvglui.core.label(self, "Hello World"),
-                lvglui.core.button(
-                    self,
-                    name="btn",
-                    children=[
-                        lvglui.core.label(self, "Click"),
-                    ],
-                ),
+                roller,
             ]
         )
